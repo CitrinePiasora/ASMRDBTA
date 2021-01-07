c6e72a5a1eb596f3fa4d56be89b17b3f436dfc9c
Citrine <48937279+CitrinePiasora@users.noreply.github.com>
2021-01-07 21:40:00 +0700

    Add files via upload

---
+++ src/Apps/Config.java
@@ -0,0 +1,28 @@
+package Apps;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Config {
+	private static Connection MySQLConfig;
+        
+	public static Connection ConfigDB() throws SQLException {
+		try {	
+			String url = "jdbc:mysql://localhost:3306/angrystanllymoviereviewapp";
+			String user = "root";
+			String password = "";
+			
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+			MySQLConfig = DriverManager.getConnection(url, user, password);
+			System.out.println("Connection Successful");
+			
+		} catch(SQLException e) {
+			System.out.println("An error has occured!");
+			e.printStackTrace();
+			
+		}
+		
+		return MySQLConfig;
+	}
+}
